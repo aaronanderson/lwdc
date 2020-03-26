91db48209261bb55439d196ece9bf01d4858bfa4
Aaron Anderson <aaronanderson@acm.org>
2020-03-25 22:42:42 -0500

    individual inputs

--- rollup.config.js
+++ rollup.config.js
@@ -7,21 +7,39 @@ import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 
 
-
-import fs from 'fs';
-
-
-const elementSrc = 'lib';
-let elementInputs = fs.readdirSync(elementSrc).filter((x) => {
-    return x !== 'global.d.ts' && x.match('.*.ts');
-});
-
-
 export default {
     external: ['react', 'react-dom'],
-    input: elementInputs,
+    input: {
+        'index': 'lib/index.ts',
+        'lwdc-button': 'lib/lwdc-button.ts',
+        'lwdc-card': 'lib/lwdc-card.ts',
+        'lwdc-checkbox': 'lib/lwdc-checkbox.ts',
+        'lwdc-combobox': 'lib/lwdc-combobox.ts',
+        'lwdc-env-label': 'lib/lwdc-env-label.ts',
+        'lwdc-fonts': 'lib/lwdc-fonts.ts',
+        'lwdc-form': 'lib/lwdc-form.ts',
+        'lwdc-form-field': 'lib/lwdc-form-field.ts',
+        'lwdc-header': 'lib/lwdc-header.ts',
+        'lwdc-icon': 'lib/lwdc-icon.ts',
+        'lwdc-icon-inject': 'lib/lwdc-icon-inject.ts',
+        'lwdc-loading': 'lib/lwdc-loading.ts',
+        'lwdc-menu-item': 'lib/lwdc-menu-item.ts',
+        'lwdc-menu': 'lib/lwdc-menu.ts',
+        'lwdc-modal': 'lib/lwdc-modal.ts',
+        'lwdc-page-header': 'lib/lwdc-page-header.ts',
+        'lwdc-popper': 'lib/lwdc-popper.ts',
+        'lwdc-radio': 'lib/lwdc-radio.ts',
+        'lwdc-select': 'lib/lwdc-select.ts',
+        'lwdc-side-panel': 'lib/lwdc-side-panel.ts',
+        'lwdc-table': 'lib/lwdc-table.ts',
+        'lwdc-tabs': 'lib/lwdc-tabs.ts',
+        'lwdc-text': 'lib/lwdc-text.ts',
+        'lwdc-textarea': 'lib/lwdc-textarea.ts',
+        'lwdc-toast': 'lib/lwdc-toast.ts',
+        'lwdc-tooltip': 'lib/lwdc-tooltip.ts',
+    },
     //input: ['lib/lwdc-card.ts', 'lib/lwdc-button.ts'],
-    preserveModules: true,
+    //preserveModules: true,
     output: {
         dir: "wc",
         format: "esm",
@@ -29,14 +47,7 @@ export default {
     },
     context: 'window',
     plugins: [
-        nodeResolve({
-            extensions: ['.ts'],
-            //resolveOnly: ['index.ts', 'lwdc-.*'],
-            customResolveOptions: {
-                basedir: elementSrc
-            }
-        }),
-
+        typescript(),
         commonjs({
             include: "node_modules/**",
             namedExports: {
@@ -47,7 +58,7 @@ export default {
             }
         }),
 
-        typescript(),
+
         resolve(),
         postcss({
             extract: false,
