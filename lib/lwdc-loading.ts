54b76e4bab5f6abd078a62d58ce510bea130c9c9
Aaron Anderson <aaronanderson@acm.org>
2020-03-31 13:14:34 -0500

    simplified loading

--- lib/lwdc-loading.ts
+++ lib/lwdc-loading.ts
@@ -10,16 +10,12 @@ export class LoadingElement extends LitElement {
 	@property({ type: Boolean, attribute: true, reflect: true })
 	loading?= false;
 
-	@property({ type: String, attribute: true, reflect: true })
-	title = '';
-
 	static get styles() {
 		return [style];
 	}
 
 	render() {
 		return html`
-		${this.title ? html`<div style="margin-right: 24px;">${this.title}</div>` : null}
 		<div class="wdc-loading-dots">		
       		<span/>
 		</div>
--- stories/lwdc-loading.stories.tsx
+++ stories/lwdc-loading.stories.tsx
@@ -6,10 +6,13 @@ import { withKnobs, text, boolean, radios } from "@storybook/addon-knobs";
 import { action } from '@storybook/addon-actions';
 
 import { loadWDCFonts } from '../lib/lwdc-fonts';
+import { coreStyle } from '../lib/lwdc-core';
+import { styleLightDOM } from '../lib/util';
 
 import '../lib/lwdc-loading';
 
 loadWDCFonts();
+styleLightDOM(document.body, coreStyle, 'lwdc-core');
 
 const center = (storyFn: () => unknown) => html`<div style="display: flex; align-items: center; justify-content: center; margin: 64px 64px;">${storyFn()}</div>`;
 
@@ -21,7 +24,7 @@ export default {
 };
 
 export const loadingStory = () => {
-	return html`<lwdc-loading loading title="Explanation"></lwdc-loading>`;
+	return html`<h3 class="wdc-type-h3" style="margin-right: 24px;">Loading</h3><lwdc-loading loading title="Explanation"></lwdc-loading>`;
 }
 loadingStory.story = {
 	name: 'Loading'
